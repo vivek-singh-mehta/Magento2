45ff3bee97e35ec9c829930394a41e2923358faa
vivek-singh-mehta <166797833+vivek-singh-mehta@users.noreply.github.com>
2024-04-15 03:15:24 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Vivekmehta_Customerimport', __DIR__);
+
